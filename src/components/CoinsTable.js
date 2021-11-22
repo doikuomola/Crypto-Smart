cb2a24c9e5c07a513b2e97423cf8be2f63d99792
doikuomola <doikuomola@gmail.com>
2021-11-22 11:17:07 -0800

    changed title heading

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -100,7 +100,7 @@ const CoinsTable = () => {
                 </TableRow>
 
               </TableHead>
-              <TableBody>
+              <TableBody>           
                 {handleSearch().slice((page - 1) * 10, (page - 1) * 10 + 10).map((row) => {
                   const profit = row.price_change_percentage_24h > 0;
                   return (
